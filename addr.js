2d7c92aee2e204db55c6f1118c6ded3203c63fda
mghs15 <40787295+mghs15@users.noreply.github.com>
2020-03-14 22:27:37 +0900

    Update addr.js

--- addr.js
+++ addr.js
@@ -4,7 +4,7 @@ var addLandcLayers = function(){
 map.on('load', function() {
   map.addSource('_addr', {
     type: 'vector',
-    tiles: ["http://localhost/200116_canoev/xyz/addr/{z}/{x}/{y}.pbf"],
+    tiles: ["https://mghs15.github.io/canoe-vector/xyz/addr/{z}/{x}/{y}.pbf"],
     "maxzoom": 9,
     "minzoom": 9
   });
@@ -24,23 +24,4 @@ map.on('load', function() {
     }
   });
 });
-}
-
-
-var landClassConvert = function(landClassTagname){
-      var landcName = ""
-      if(landClassTagname === 'sanchi'){
-          landcName = "山地";
-      }else if(landClassTagname === 'daichi'){
-          landcName = "台地";
-      }else if(landClassTagname === 'sakyu'){
-          landcName = "砂丘";
-      }else if(landClassTagname === 'shizenteibou'){
-          landcName = "自然堤防";
-      }else if(landClassTagname === 'kouhaishicchi'){
-          landcName = "後背湿地";
-      }else{
-          landcName = "氾濫平野など";
-      }
-      return landcName ;
 }
